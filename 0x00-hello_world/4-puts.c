8a2a44b1a52e43c09c4faa5b8e1547fac4ec73e2
Donald Ajaps <ajapon@yahoo.com>
2022-09-08 21:37:24 +0000

    Do Task 0x00.04.Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* Declare variable and assign output string to it */
+	char myChar[] = "\"Programming is like building a multilingual puzzle";
+
+	/* writing the string to stdout */
+	puts(myChar);
+
+	/* return 0 to end program */
+	return (0);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This repo contains the results of running the tasks in the "C - Hello, World" Project in Week One of the ALX SE course.
