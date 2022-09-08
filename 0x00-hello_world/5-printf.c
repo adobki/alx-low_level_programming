e7ab45701df886839e63782e64df3feaec20b205
Donald Ajaps <ajapon@yahoo.com>
2022-09-08 21:55:55 +0000

    Do Task 0x00.05.Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* Declare variable and assign output string to it */
+	char myChar[] = "with proper grammar, but the outcome is a piece of art,";
+
+	/* Output string to screen (stdout) */
+	printf("%s\n", myChar);
+
+	/* return 0 to end program */
+	return (0);
+}
