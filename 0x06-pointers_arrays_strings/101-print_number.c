b6da514d62d05f7838d8334f76abb493884c5cb2
Donald Ajaps <ajapon@yahoo.com>
2022-10-13 18:20:18 -0700

    Do Task 0x06.[ADVANCED].9.Numbers have life...

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * print_number - Function that prints an integer using only _putchar.
+ *
+ * @n: Integer to be printed.
+ */
+void print_number(int n)
+{
+	int n2 = n;
+	int length = 0;
+	int countr;
+
+	/* Check if given input is zero. Print zero and exit if true */
+	if (n == 0)
+	{
+		_putchar(n + 48);
+		return;
+	}
+
+	/* Check if given input is negative and fix it */
+	if (n < 0)
+	{
+		_putchar(45);
+		n *= -1;
+		n2 = n;
+	}
+
+	/* Find total number of digits in given integer */
+	while (n2 != 0)
+	{
+		n2 /= 10;
+		++length;
+	}
+
+	while (length)
+	{
+		/* Get first digit from integer*/
+		n2 = n;
+		for (countr = 1; countr < length; countr++)
+			n2 /= 10;
+
+		/* Print and remove the digit from given integer */
+		_putchar(n2 + 48);
+		for (countr = 1; countr < length; countr++)
+			n2 *= 10;
+		n -= n2;
+
+		length--;
+	}
+}
+
