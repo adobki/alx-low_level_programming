51888fe577af2a3c027c5833758c14791f253759
Donald Ajaps <ajapon@yahoo.com>
2022-10-30 15:11:16 +0100

    Do Task 0x0C.4 and Fix Comment in Task 0x0C.3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
 #ifndef ABS_
 #define ABS_
 #define ABS(num) ((num) < 0 ? (num) * -1 : (num))
-#endif /* #ifndef ABS(num) */
+#endif /* #ifndef ABS_ */
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,4 @@
+#ifndef SUM_
+#define SUM_
+#define SUM(num1, num2) ((num1) + (num2))
+#endif /* #ifndef SUM_ */
